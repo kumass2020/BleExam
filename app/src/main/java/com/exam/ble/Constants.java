cb0ea80030d138d376cbde92b96d687e38dda987
kumass2020 <kumass2021@gmail.com>
2021-07-08 16:28:52 +0900

    1

--- app/src/main/java/com/exam/ble/Constants.java
+++ app/src/main/java/com/exam/ble/Constants.java
@@ -10,8 +10,12 @@ public class Constants {
     // scan period in milliseconds
     public final static int SCAN_PERIOD = 5000;
 
-    public static String SERVICE_STRING = "CB660002-4339-FF22-A1ED-DEBFED27BDB4";
+//    public static String SERVICE_STRING = "CB660002-4339-FF22-A1ED-DEBFED27BDB4";
+    public static String SERVICE_STRING = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
     public static final UUID SERVICE_UUID = UUID.fromString(SERVICE_STRING);
-    public static String CHARACTERISTIC_UUID = "CB660004-4339-FF22-A1ED-DEBFED27BDB4";
-    public static String CONFIG_UUID = "00005609-0000-1001-8080-00705c9b34cb";
+//    public static String CHARACTERISTIC_UUID = "CB660004-4339-FF22-A1ED-DEBFED27BDB4";
+    public static String CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+//    public static String CONFIG_UUID = "00005609-0000-1001-8080-00705c9b34cb";
+    public static String CONFIG_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+
 }
